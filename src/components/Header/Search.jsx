9437ad31f81fc659b6a17622d4c0ff3f9737b5af
Kececi <kececirecep@outlook.com>
2024-05-07 19:19:49 +0300

    form

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -8,6 +8,7 @@ const Search = () => {
         className='border border-my-gray outline-none p-2 w-[240px]' />
       <button className='bg-my-pink p-2 text-white'><CiSearch  size="26px"/></button>
     </form>
+    
   )
 }
 
